24cc94b220004cc0af1fc4707c2fb109f07298d5
macdonald34 <macdonald.muhavi@student.moringaschool.com>
2023-12-13 20:54:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+} if (distance <= 400) {
+  return "This one is on me";
+} else if (distance > 400 && distance <= 2000){
+  return "I will gladly take your thirty bucks.";
+} else if (distance > 2000 && distance <== 2500) {
+  return "I will gladly take your thirty bucks."; 
+} else {
+  return "No can do.";
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good."
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch (Tip) {
+    case 'generous':
+      return "Thank you so much.";
+      case 'not generous' :
+        return "Thank you."
+        default :
+        return "Bye."
+  }
 }
